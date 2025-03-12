f1c883a1b6d81143433ec79eb4f67bae077f81b2
bxrkxnt <berkantdoguer@gmail.com>
2025-03-12 16:28:49 +0000

    working lates time alg program

--- problem_solving/latest_time.py
+++ problem_solving/latest_time.py
@@ -4,19 +4,22 @@
 def get_first_digit(digits: list[int]) -> int:
 
     optional_digits = []
+    optional_digits_one = []
     for digit in digits:
+        optional_digits.append(digit)
         if digit == 0 or digit == 1 or digit == 2:
-            optional_digits.append(digit)
+            optional_digits_one.append(digit)
 
-    first_digit = max(optional_digits)
+    first_digit = max(optional_digits_one)
     optional_digits.remove(first_digit)
     return first_digit, optional_digits
 
 
 def get_second_digit(digits: list[int], first_digit: int) -> int:
 
-    optional_digits = [x for x in digits if x in range(0, 9)]
-    print(optional_digits)
+    optional_digits = [x for x in digits if x <= 9]
+    # print(optional_digits)
+
     if first_digit == 2:
         optional_digits_two = []
         for digit in optional_digits:
@@ -35,8 +38,14 @@ def get_second_digit(digits: list[int], first_digit: int) -> int:
 
 def get_third_digit(digits: list[int]) -> int:
 
-    optional_digits = [x for x in digits if x in range(0, 6)]
-    third_digit = max(optional_digits)
+    optional_digits = [digit for digit in digits]
+    optional_digits_third = [x for x in digits if x <= 6]
+
+    # for debugging
+    # print(optional_digits)
+    # print(optional_digits_third)
+
+    third_digit = max(optional_digits_third)
     optional_digits.remove(third_digit)
 
     return third_digit, optional_digits
@@ -44,14 +53,14 @@ def get_third_digit(digits: list[int]) -> int:
 
 def get_fourth_digit(digits: list[int]) -> int:
     if len(digits) <= 1:
-        fourth_digit = digits[-1]
+        fourth_digit = digits[0]
 
     return fourth_digit
 
 
 if (__name__ == "__main__"):
 
-    first, first_list_remaining = get_first_digit([1, 9, 0, 8])
+    first, first_list_remaining = get_first_digit([2, 5, 7, 0])
 
     second, second_list_remaining = get_second_digit(
         first_list_remaining, first)
@@ -61,4 +70,4 @@ if (__name__ == "__main__"):
     fourth = get_fourth_digit(third_list_remaining)
 
     print(
-        "The latest time with the combination given is: {first}{second}:{third}{fourth}")
+        f"The latest time with the combination given is: {first}{second}:{third}{fourth}")
