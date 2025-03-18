2cc3e87036a3955a771543162b52a8ede023d30e
bxrkxnt <berkantdoguer@gmail.com>
2025-03-18 10:35:24 +0000

    commit files

--- problem_solving/test_latest_time.py
+++ problem_solving/test_latest_time.py
@@ -4,16 +4,24 @@ from latest_time import (get_first_digit,
                          get_second_digit,
                          get_third_digit)
 
+
 def test_get_first_digit_returns_zero():
     assert get_first_digit([0, 9, 3, 8]) == 0
+
 
 def test_get_first_digit_returns_one():
     assert get_first_digit([1, 9, 8, 3]) == 1
 
+
 def test_get_first_digit_returns_two():
     assert get_first_digit([9, 1, 2, 5]) == 2
 
-@pytest.mark.parametrize("digits,second,expected", 
+
+def test_get_third_digit_returns_five():
+    assert get_third_digit([5, 1,]) == 5
+
+
+@pytest.mark.parametrize("digits,second,expected",
                          [([9, 1, 5], 2, 5),
                           ([9, 8, 3], 1, 9)])
 def test_get_second_digit_returns_five(digits, second, expected):
